e1751d0138965f966e178b498352235489b8a46e
PvPisistratus <pvpisistratus@gmail.com>
2020-04-25 18:04:02 -0500

    Exported nmr_params struct and constructor

--- src/SpectraFit.jl
+++ src/SpectraFit.jl
@@ -2,6 +2,6 @@ module SpectraFit
 
 include("boron_10_fitting.jl")
 
-export get_experimental, fit_nmr, generate_theoretical_spectrum
+export get_experimental, fit_nmr, generate_theoretical_spectrum, nmr_params
 
 end # module
