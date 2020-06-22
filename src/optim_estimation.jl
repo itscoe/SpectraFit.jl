606631a4cd13144f0bbca2f431a31f48f6996cf6
PvPisistratus <pvpisistratus@gmail.com>
2020-06-22 14:10:43 -0500

    Rescaled so ecdf always falls between 0 and 1

--- src/optim_estimation.jl
+++ src/optim_estimation.jl
@@ -18,6 +18,8 @@ function ols_cdf(
     transitions::UnitRange{Int64} = 1:(2*I),
 )
     theoretical_ecdf = ecdf(estimate_powder_pattern(parameters, samples, ν0, I, transitions = transitions)).(experimental[:, 1])
+    theoretical_ecdf .-= theoretical_ecdf[1]
+    theoretical_ecdf .*= 1 / theoretical_ecdf[end]
     return sum((experimental_ecdf - theoretical_ecdf) .^ 2)
 end
 
