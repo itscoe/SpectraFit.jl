c5f2f799f86229104dcbc4fc88f923a7a43ee0e6
PvPisistratus <pvpisistratus@gmail.com>
2020-07-05 21:35:15 -0500

    Added Distributions to chemical_shift_params file

--- src/chemical_shift_params.jl
+++ src/chemical_shift_params.jl
@@ -1,3 +1,5 @@
+using Distributions
+
 struct chemical_shift_params
     σᵢₛₒ::Normal
     Δσ::Normal
@@ -5,7 +7,7 @@ struct chemical_shift_params
 end
 
 function chemical_shift_params(p::Array{Float64})
-    return chemical_shift_params(Normal(p[1], max(0.0, p[2])), 
+    return chemical_shift_params(Normal(p[1], max(0.0, p[2])),
         Normal(p[3], max(0.0, p[4])),
         truncated(Normal(clamp(p[5], 0.001, 0.999), max(0.0, p[6])), 0.0, 1.0))
 end
