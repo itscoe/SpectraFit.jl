29f3e5b943174f551885aaabe4d0565effa69b09
PvPisistratus <pvpisistratus@gmail.com>
2020-07-10 19:01:54 -0500

    split forward_model.jl into ChemicalShift.jl and Quadrupolar.jl

--- src/ChemicalShift.jl
+++ src/ChemicalShift.jl
@@ -1,5 +1,8 @@
 using Distributions
 
+const cos2α_dist = Uniform(-1, 1)
+const sinβ_dist = Uniform(-1, 1)
+
 struct ChemicalShift
     σᵢₛₒ::Normal
     Δσ::Normal
@@ -11,3 +14,22 @@ function ChemicalShift(p::Array{Float64})
         Normal(p[3], max(0.0, p[4])),
         truncated(Normal(clamp(p[5], 0.001, 0.999), max(0.0, p[6])), 0.0, 1.0))
 end
+
+function get_ν(
+    α::Float64,
+    β::Float64,
+    σᵢₛₒ::Normal,
+    Δσ::Normal,
+    ησ::Truncated{Normal{Float64},Continuous,Float64},
+)
+    Δσ_sample = rand(Δσ)
+    σᵢₛₒ_sample = rand(σᵢₛₒ)
+    ησ_sample = rand(ησ)
+    σᵢₛₒ_sample - (Δσ_sample / 3) * (3 * (1 - β^2) - 1 - ησ_sample * β^2 * α);
+end
+
+function get_powder_pattern(params::ChemicalShift, N::Int64)
+    α = rand(cos2α_dist, N)
+    β = rand(sinβ_dist, N)
+    return get_ν.(α, β, params.σᵢₛₒ, params.Δσ, params.ησ)
+end;
--- src/SpectraFit.jl
+++ src/SpectraFit.jl
@@ -5,12 +5,12 @@ ENV["GKS_ENCODING"] = "utf-8"
 include("Quadrupolar.jl")
 include("ChemicalShift.jl")
 include("utility_functions.jl")
-include("forward_model.jl")
 include("optim_estimation.jl")
 include("bayesian_estimation.jl")
 
 export get_experimental,
        Quadrupolar,
+       ChemicalShift,
        fit_nmr,
        metropolis_hastings,
        compare_theoreticals,
