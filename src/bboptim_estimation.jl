7c386fb1461aa54bf0ddedb39df4c38e63aca040
itscoe <itslagle@coe.edu>
2021-01-11 22:06:46 -0600

    Added multi site black box chemical shift estimation, I think

--- src/bboptim_estimation.jl
+++ src/bboptim_estimation.jl
@@ -37,12 +37,12 @@ function fit_chemicalshift_bb(
     experimental::Array{Float64, 2};
     sites::Int64 = 1,
     max_func_evals::Int64 = 10000,
-    method::Symbol = :seperable_nes,
+    method::Symbol = :xnes,
     samples::Int64 = 1_000_000
 )
     experimental_ecdf = get_experimental_ecdf(experimental)
     ν0 =  get_ν0(experimental, experimental_ecdf)
-    search_range = [(-4000.0, 4000.0), (0.000001, 800.0), (-4000.0, 4000.0), (0.000001, 400.0), (0.000001, 1.0), (0.000001, 1.0),  (0.000001, 1.0)]
+    search_range = repeat([(-4000.0, 4000.0), (0.000001, 800.0), (-4000.0, 4000.0), (0.000001, 400.0), (0.000001, 1.0), (0.000001, 1.0),  (0.000001, 1.0)], sites)
 
     fitness_progress_history = Array{Tuple{Int, Float64},1}()
     callback = oc -> push!(fitness_progress_history,
