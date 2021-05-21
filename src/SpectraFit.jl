a50427141f3b23797c4ccd4aa0c975cb565e2600
itscoe <itslagle@coe.edu>
2021-05-21 13:03:20 -0500

    added what should be a proper function call limit for each algorithm

--- src/SpectraFit.jl
+++ src/SpectraFit.jl
@@ -6,23 +6,15 @@ include("Quadrupolar.jl")
 include("ChemicalShift.jl")
 include("utility_functions.jl")
 include("optimization.jl")
-#include("bayesian_estimation.jl")
-#include("genetic_algorithm.jl")
-#include("bboptim_estimation.jl")
+include("bayesian_estimation.jl")
 
 export get_experimental,
        Quadrupolar,
        ChemicalShift,
-       #fit_quadrupolar,
-       #fit_chemical_shift,
+       quadrupolar_opt,
        metropolis_hastings,
        compare_theoreticals,
        get_data,
        plot_chemical_shift
-       #get_output_table,
-       #genetic_algorithm,
-       #genetic_algorithm_csa,
-       #fit_quadrupolar_bb,
-       #fit_chemicalshift_bb
 
 end # module
