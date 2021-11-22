014fa11e83aac9087c595478c71d0dcbb7d159a4
itscoe <itslagle@coe.edu>
2021-11-22 16:01:52 -0600

    Don't use UInt's in internal operations

--- src/experimental/FPOT.jl
+++ src/experimental/FPOT.jl
@@ -9,25 +9,25 @@ FPOT(n::Int, d::Int) = FPOT(Int8(n), UInt8(d))
 FPOT(n::Int) = FPOT(Int8(n), 0x00)
 
 # Addition
-@inline Base.:+(a::FPOT, b::FPOT) = 
-    a.d == b.d ? FPOT(a.n + b.n, a.d) : 
-                 FPOT(a.n * (0x01 << b.d) + b.n * (0x01 << a.d), a.d + b.d)
-@inline Base.:+(a::FPOT, b::Int64) = FPOT(a.n + b * (0x01 << a.d), a.d)
-@inline Base.:+(a::FPOT, ::Val{1}) = FPOT(a.n + 0x01 << a.d, a.d)
+@inline Base.:+(a::FPOT, b::FPOT) = a.d == b.d ? FPOT(a.n + b.n, a.d) : 
+    FPOT(a.n * (Int8(1) << b.d) + b.n * (Int8(1) << a.d), a.d + b.d)
+@inline Base.:+(a::FPOT, b::Int64) = FPOT(a.n + b * (Int8(1) << a.d), a.d)
+@inline Base.:+(a::FPOT, ::Val{1}) = FPOT(a.n + Int8(1) << a.d, a.d)
 @inline Base.:+(a::FPOT, ::Val{FPOT(3, 1)}) = a.d == 1 ? FPOT(a.n + 3, a.d) : 
-    FPOT(a.n * 0x02 + 3 * (0x01 << a.d), a.d + 1)
+    FPOT(a.n * Int8(2) + 3 * (Int8(1) << a.d), a.d + 1)
 @inline Base.:+(b::Int64, a::FPOT) = a + b
 
 # Subtraction
 @inline Base.:-(a::FPOT) = FPOT(-a.n, a.d)
 @inline Base.:-(a::FPOT, b::FPOT) = 
-    a.d == b.d ? FPOT(a.n + b.n, a.d) : 
-                 FPOT(a.n * (0x01 << b.d) - b.n * (0x01 << a.d), a.d + b.d)
-@inline Base.:-(a::FPOT, b::Int64) = FPOT(a.n - b * (0x01 << a.d), a.d)
-@inline Base.:-(b::Int64, a::FPOT) = FPOT(b * (0x01 << a.d) - a.n, a.d)
-@inline Base.:-(a::FPOT, ::Val{1}) = FPOT(a.n - (0x01 << a.d), a.d)
-@inline Base.:-(a::FPOT, ::Val{FPOT(1, 1)}) = a.d == 1 ? FPOT(a.n + 1, a.d) : 
-    FPOT(a.n * 0x02 - (0x01 << a.d), a.d + 1)
+    a.d == b.d ? FPOT(a.n - b.n, a.d) : 
+                 FPOT(a.n * (Int8(1) << b.d) - b.n * (Int8(1) << a.d), a.d + b.d)
+@inline Base.:-(a::FPOT, b::Int64) = FPOT(a.n - b * (Int8(1) << a.d), a.d)
+@inline Base.:-(b::Int64, a::FPOT) = FPOT(b * (Int8(1) << a.d) - a.n, a.d)
+@inline Base.:-(a::FPOT, ::Val{1}) = FPOT(a.n - (Int8(1) << a.d), a.d)
+@inline Base.:-(a::FPOT, ::Val{FPOT(1, 1)}) = a.d == 0x01 ? 
+    FPOT(a.n + Int8(1), a.d) : 
+    FPOT(a.n * 0x02 - (Int8(1) << a.d), a.d + 1)
 
 # Multiplication
 @inline Base.:*(a::FPOT, b::FPOT) = FPOT(a.n * b.n, a.d + b.d)
@@ -58,4 +58,4 @@ Base.:<=(b::Int64, a::FPOT) = a.d == 0x00 ? b <= a.n : b <= Float64(a)
 
 # Rounding
 Base.round(a::SpectraFit.FPOT, _::RoundingMode{:Down}) = 
-    FPOT(a.n ÷ (0x01 << a.d), 0x00)
+    FPOT(a.n ÷ (Int8(1) << a.d), 0x00)
