69a6b417e6a48a03afc04642df7b7ee7ceb9b79a
itscoe <itslagle@coe.edu>
2021-11-22 15:53:55 -0600

    Fixed subtraction of FPOT's and Integers

--- src/experimental/FPOT.jl
+++ src/experimental/FPOT.jl
@@ -24,7 +24,7 @@ FPOT(n::Int) = FPOT(Int8(n), 0x00)
     a.d == b.d ? FPOT(a.n + b.n, a.d) : 
                  FPOT(a.n * (0x01 << b.d) - b.n * (0x01 << a.d), a.d + b.d)
 @inline Base.:-(a::FPOT, b::Int64) = FPOT(a.n - b * (0x01 << a.d), a.d)
-@inline Base.:-(a::FPOT, b::Int64) = FPOT(b * (0x01 << a.d) - a.n, a.d)
+@inline Base.:-(b::Int64, a::FPOT) = FPOT(b * (0x01 << a.d) - a.n, a.d)
 @inline Base.:-(a::FPOT, ::Val{1}) = FPOT(a.n - (0x01 << a.d), a.d)
 @inline Base.:-(a::FPOT, ::Val{FPOT(1, 1)}) = a.d == 1 ? FPOT(a.n + 1, a.d) : 
     FPOT(a.n * 0x02 - (0x01 << a.d), a.d + 1)
